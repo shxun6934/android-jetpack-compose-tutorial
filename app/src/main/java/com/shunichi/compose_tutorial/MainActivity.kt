21378adca4366322b346665e2d83e14055ee5c89
shxn6934 <b1612851@planet.kanazawa-it.ac.jp>
2022-06-04 18:32:20 +0900

    Create MessageCard component & display preview

--- app/src/main/java/com/shunichi/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/shunichi/compose_tutorial/MainActivity.kt
@@ -4,12 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            // Text("Hello world!")
+            MessageCard("Android")
         }
     }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android")
 }
