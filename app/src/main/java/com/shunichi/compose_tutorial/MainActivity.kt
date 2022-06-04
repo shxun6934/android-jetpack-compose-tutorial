8a7fb6c8a4493e9121945b144a2be03d288baaa0
shxn6934 <b1612851@planet.kanazawa-it.ac.jp>
2022-06-04 19:23:55 +0900

    Add Animation

--- app/src/main/java/com/shunichi/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/shunichi/compose_tutorial/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,12 +23,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.shunichi.compose_tutorial.ui.data.SampleData
 import com.shunichi.compose_tutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +43,8 @@ class MainActivity : ComponentActivity() {
             // Text("Hello world!")
             // MessageCard("Android")
             ComposeTutorialTheme {
-                MessageCard(Message("Android", "Jetpack Compose"))
+                // MessageCard(Message("Android", "Jetpack Compose"))
+                Conversation(SampleData.conversationSample)
             }
         }
     }
@@ -62,7 +71,12 @@ fun MessageCard(msg: Message) {
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 fontSize = 12.sp,
@@ -70,12 +84,20 @@ fun MessageCard(msg: Message) {
                 style = MaterialTheme.typography.subtitle2
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     fontSize = 14.sp,
                     modifier = Modifier.padding(all = 4.dp),
-                    style = MaterialTheme.typography.body2
+                    style = MaterialTheme.typography.body2,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                 )
             }
         }
@@ -96,8 +118,9 @@ fun Conversation(messages: List<Message>) {
 @Composable
 fun PreviewMessageCard() {
     ComposeTutorialTheme {
-        MessageCard(
-            msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-        )
+        // MessageCard(
+        //     msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+        // )
+        Conversation(SampleData.conversationSample)
     }
 }
